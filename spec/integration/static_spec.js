37123368b60da4f0a6080e13ea71fe4f92a476cf
Curtis Bloomfield <curtbloom@gmail.com>
2019-08-20 23:04:16 -0400

    express-assignment done

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -20,4 +20,17 @@ describe("routes : static", () => {
     });
 
   });
+
+  describe("GET /marco", () => {
+
+    it("should return status code 200", (done) => {
+
+      request.get(base + 'marco', (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe("polo");
+
+        done();
+      });
+    });
+  });
 });
