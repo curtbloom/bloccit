bd2b3291f48bd982e6896e5fa2c28c1cc21b8b40
Curtis Bloomfield <curtbloom@gmail.com>
2019-08-30 09:43:27 -0400

    routing assignment done

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -19,8 +19,8 @@ describe("routes : static", () => {
   });
 
   describe("GET /about", () => {
-    it('should return status code of 200 and have "About Us" in the body of the response', (done) => {
-        request.get(base + 'about', (err, res, body) => {
+    it("should return status code 200 and have About Us string in the body", (done) => {
+        request.get(base, (err, res, body) => {
           expect(res.statusCode).toBe(200);
           expect(body).toContain("About Us");
           done();
